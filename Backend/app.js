0d1016f644911937dc5acea070523f71dc56b747
raushangiri <129365523+raushangiri@users.noreply.github.com>
2023-05-02 23:30:57 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: '*.onrender.com,*.netlify.app',
+  origin: '*.netlify.app',
   credentials: true,
 }));
 // router page required here
