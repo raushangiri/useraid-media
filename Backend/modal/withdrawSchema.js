2ae7ce9916aeb8020273fd0543b2a6ea4f5d5796
raushangiri <129365523+raushangiri@users.noreply.github.com>
2023-05-04 16:54:13 +0530

    Create withdrawSchema.js

---
+++ Backend/modal/withdrawSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const withdrawSchema = new mongoose.Schema({
+  customer_id: { type: Number, required: true },
+  requestType: { type: String, required: true },
+  qr_code_screenshot: {},
+  amount: { type: Number, required: Number },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const WithdrawSchema = mongoose.model("withdraw_master", withdrawSchema);
+
+module.exports = WithdrawSchema;
